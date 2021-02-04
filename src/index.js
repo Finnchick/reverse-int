8ed8e31352d752b24f93ff4fc70a9097e94f7f84
Finnchick <sergik3001@gmail.com>
2021-02-04 19:39:00 +0300

    implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+    if (String(n).startsWith('-')) {
+        return  (String(n).split('').slice(1, String(n).length).reverse().join(''));
+    } else {
+        return  (String(n).split('').reverse().join(''));
+    }
   
 }
